19720a520fadc3f1bfb2fcc58108c0262538b961
Heesang, Seo <chichs3s@gmail.com>
2022-02-09 15:41:56 +0900

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,32 @@
+const promise1 = () => {
+  let data = [1, 2, 3, 4, 5];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('promise 1sec');
+      if (data.length == 4) {
+        reject(new Error('errrrrrr'));
+      } else {
+        resolve(data);
+      }
+    }, 1000);
+  });
+};
+
+const f = () => {
+  return promise1().then((result) => {
+    return result;
+  });
+};
+
+(async () => {
+  console.log('___ start');
+
+  try {
+    let tmp = await f();
+    console.log(tmp);
+  } catch (e) {
+    console.log('err', e);
+  }
+
+  console.log('___ end');
+})();
