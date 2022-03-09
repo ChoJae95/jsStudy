3c56fe3f505eef91333a7768102423f0342d586b
cho950808 <cho950808@naver.com>
2022-03-09 17:32:41 +0900

    cancat / fillter study

---
+++ jsES6/cancat.js
@@ -0,0 +1,8 @@
+// ------cancat -------//
+// 기존 배열을 토대로 변경한 새로운 배열이 리턴됨
+
+const array = [1, 2];
+let result = array.concat(4);
+
+console.log(array); // 결과 : [1, 2]
+console.log(result); // 결과 : [1, 2, 4]
--- jsES6/filter.js
+++ jsES6/filter.js
@@ -6,3 +6,28 @@ const filterd = numbers.filter(number => number > 2);
 
 console.log(filterd); // 결과 : 3 4 5
 
+const numbers1 = [-3, -2, -1, 0 , 1, 2, 3];
+const nextNumbers = numbers1.map(number => number === 0 ? 10 : number);
+
+console.log(nextNumbers);
+
+const items = [
+  {
+    id: 1,
+    text: "안녕하세요.",
+  },
+  {
+    id: 2,
+    text: "환영합니다.",
+  },
+  {
+    id: 3,
+    text: "반갑습니다.",
+  },
+];
+
+const nextItems = items.map(item => item.id === 2? {...item, text:'ㅎㅇㅎㅇ'} : item);
+
+console.log(nextItems);
+
+
