a28231d123b0603ead21a6a56b62961714167328
cho950808 <cho950808@naver.com>
2022-03-09 15:50:19 +0900

    filter study

---
+++ jsES6/filter.js
@@ -0,0 +1,8 @@
+// ------filter -------// 
+// 배열에서 특정 항목을 제거해 새로운 배열을 만듦
+
+const numbers = [1,2,3,4,5];
+const filterd = numbers.filter(number => number > 2);
+
+console.log(filterd); // 결과 : 3 4 5
+
